94711769498722b08d760322c6a798663fd6a1a5
naser13 <naser.aliabadi@gmail.com>
2015-03-19 23:47:54 +0330

    added fixtures for cities and provinces

--- address/models.py
+++ address/models.py
@@ -8,7 +8,13 @@ class Address(models.Model):
 class Province(models.Model):
     name = models.CharField(max_length=100, default='تهران')
 
+    def __str__(self):
+        return self.name
+
 
 class City(models.Model):
     name = models.CharField(max_length=100, default='تهران')
     province = models.ForeignKey(Province, default=1)
+
+    def __str__(self):
+        return self.name
